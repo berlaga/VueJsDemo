4960911658e930ed4ee3f4bdff8060625c31123f
Alexey Geller <alexey.geller@ontario.ca>
2018-02-17 12:18:21 -0500

    Add project files.

---
+++ VueJsDemo/apps/components/date-picker/date-picker.js
@@ -0,0 +1,80 @@
+﻿
+//var bus = new Vue();
+
+Vue.component('date-picker', {
+
+    template: "<input />",
+
+    props: ['dateFormat', 'elId','somedate'],
+
+    data()
+    {
+        return {
+            id: null
+        }
+    }, 
+    watch:
+    {
+        somedate: function (newVal, oldVal)
+        {
+            console.log(oldVal);
+            console.log(newVal);
+
+            $(this.$el).datepicker("setDate", newVal);
+        }
+
+    },
+    mounted: function ()
+    {
+        var self = this;
+        this.$el.id = this.elId;
+
+        $(this.$el).datepicker({
+
+            dateFormat: this.dateFormat,
+            onSelect: function (date)
+            {
+                self.$emit('update-date', date);
+            }
+        });
+
+        self.id = this._uid + 1;
+
+        //bus.$on('date-changed', function (date)
+        //{
+        //    console.log(date);
+        //    $("#dp1").datepicker("setDate", date);
+        //    var currentDate = $("#dp1").datepicker("getDate");
+        //})
+
+
+    },
+    beforeDestroy: function ()
+    {
+        $(this.$el).datepicker('hide').datepicker('destroy');
+    }
+});
+
+new Vue({
+
+    el: '#app',
+
+    data: {
+
+        date: null
+    },
+
+    methods: {
+
+        updateDate: function (date)
+        {
+            this.date = date;
+        },
+        setDate: function (event)
+        {
+            this.date = new Date("2018-02-11");
+            //bus.$emit('date-changed', this.date);
+        }
+    }
+});
+
---
+++ VueJsDemo/vue_demo.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vue_demo.aspx.cs" Inherits="VueJsDemo.vue_demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Demo</title>
+    
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/Site.css" rel="stylesheet" />
+    <link href="Content/themes/base/all.css" rel="stylesheet" />
+
+
+    <script src="Scripts/jquery-3.3.1.js"></script>
+    <script src="Scripts/jquery-ui-1.12.1.js"></script>
+    <script src="Scripts/bootstrap.js"></script>
+
+    <script src="Scripts/vue.js"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div  class="container">
+
+            <div class="row">
+                <div class="col-1">
+                    test
+                </div>
+                <div id="app" class="col-3">
+                  <date-picker @update-date="updateDate" el-id="abc" v-bind:somedate="date" date-format="yy-mm-dd">
+                  </date-picker>
+                    <p>{{ date }}</p>    
+                    <button type="button" v-on:click="setDate">Set date</button>
+
+                </div>
+
+            </div>
+
+        </div>
+    </form>
+
+</body>
+
+    <script src="apps/components/date-picker/date-picker.js"></script>
+
+</html>
